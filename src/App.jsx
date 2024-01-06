b860b8245813abd4083be1fd907c88b33608e46d
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-01-06 16:46:37 +0530

    added few more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Cards from "./Cards";
 
 const batch = "B52WETAMIL";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let count = 0;
+
+  const incCount = () => {
+    count++;
+  };
 
   return (
     <>
-      <div>
+      <div
+        style={{
+          border: "2px solid red",
+          backgroundColor: "yellow",
+        }}
+      >
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -20,11 +29,11 @@ function App() {
         <label htmlFor="sample-inp"></label>
         <input id="sample-inp" />
       </div>
-      <h1>Vite + React + {batch} + Mentor</h1>
+      <h1>
+        Vite, React, {batch}, Mentor, {2 + 4}
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={incCount}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -32,6 +41,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {/* <Cards /> */}
+      <Cards title="B52WETAMIL PROPS" date="2024-01-06">
+        Test Content Inside the Cards Component
+      </Cards>
+      <Cards title="B49WEENG PROPS" date="2023-12-24">
+        <h1>Test Content on the Second Cards Tag</h1>
+      </Cards>
+      <Cards title="B42WEENG" date="2023-10-12" />
     </>
   );
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,5 +1,10 @@
 // Create a new component called cards
-import "./Cards.css";
+import PropTypes from "prop-types";
+// normal css import
+// import './Cards.css';
+
+// Module CSS
+import styles from "./Cards.module.css";
 
 const data = [
   {
@@ -25,7 +30,41 @@ const data = [
   },
 ];
 
-const Cards = () => {
+// Senior - Yello Star
+// Junior - Maroon Star
+// Mid Level - Pink Star
+
+// Getting rest all of the props
+const Card = ({ name, image, role, ...restProps }) => {
+  const handleClick = (e) => {
+    console.log(e);
+    console.log("Clicking");
+  };
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={name} />
+      <h1>{name} </h1>
+      <h2>Role: {role} </h2>
+      {/* Conditional Rendering */}
+      {role === "Senior" && <i className="fa-solid fa-star"></i>}
+      {console.log("rest props", restProps)}
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  role: PropTypes.string,
+};
+
+const Cards = (props) => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitting################################");
+  };
+
   return (
     // inline styling
     <div
@@ -33,25 +72,59 @@ const Cards = () => {
         margin: 16,
         // margin: "16px"
       }}
-      className="cards"
+      className={styles.container}
     >
-      <div className="card">
-        <img src={data[0].image} alt={data[0].name} />
-        <h1>{data[0].name} </h1>
-        <h2>Role: {data[0].role} </h2>
-      </div>
-      <div className="card">
-        <img src={data[1].image} alt={data[1].name} />
-        <h1>{data[1].name} </h1>
-        <h2>Role: {data[1].role} </h2>
-      </div>
-      <div className="card">
-        <img src={data[2].image} alt={data[2].name} />
-        <h1>{data[2].name} </h1>
-        <h2>Role: {data[2].role} </h2>
-      </div>
+      <h1>{props.title}</h1>
+      <h2>Date: {props.date}</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="date">Date:</label>
+        <input type="date" name="date" id="date" value={props.date} />
+        <input type="submit" name="submit" />
+      </form>
+      {/* <Card name={data[0].name} image={data[0].image} role={data[0].role} />
+      <Card name={data[1].name} image={data[1].image} role={data[1].role} />
+      <Card name={data[2].name} image={data[2].image} role={data[2].role} /> */}
+      {/* <Card {...data[2]} /> */}
+      {/* working with the list */}
+      {/* {[
+        <Card
+          key={data[0].name}
+          name={data[0].name}
+          image={data[0].image}
+          role={data[0].role}
+        />,
+        <Card
+          key={data[1].name}
+          name={data[1].name}
+          image={data[1].image}
+          role={data[1].role}
+        />,
+        <Card
+          key={data[2].name}
+          name={data[2].name}
+          image={data[2].image}
+          role={data[2].role}
+        />,
+      ]} */}
+
+      {data.map((cardData) => (
+        <Card
+          // Do not use index as key
+          key={cardData.name}
+          name={cardData.name}
+          image={cardData.image}
+          role={cardData.role}
+        />
+      ))}
+      <div>{props.children}</div>
     </div>
   );
+};
+
+Cards.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
 export default Cards;
